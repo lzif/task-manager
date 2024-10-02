ac0a93ef9f5c7b314488fbf765a5e2488d01ca8d
lzif <lukyfriendly@gmail.com>
2024-10-02 11:00:00 +0700

    Add task listing functionality

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const tasks = [];
 function addTask(title) {
     tasks.push({ title, completed: false });
 }
+function listTasks() {
+    tasks.forEach((task, index) => {
+        console.log(`${index + 1}. [${task.completed ? 'X' : ' '}] ${task.title}`);
+    });
+}
